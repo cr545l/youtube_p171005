a622a22b47ad28f0731ae5b59649386faa01c843
cr545l <cr545l@gmail.com>
2017-10-06 09:39:22 +0900

    https://youtu.be/3kujUUlolB0

---
+++ Assets/Example.cs
@@ -0,0 +1,98 @@
+﻿using Lofle;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example : MonoBehaviour
+{
+	private StateMachine _stateMachine = null;
+	private StateMachine<Example> _ownerStateMachine = null;
+
+	//public string Foo => "Bar";
+		
+	private void Start ()
+	{
+		// 기본 상태머신
+		_stateMachine = new StateMachine();
+
+		// 모든 상태 클래스 내부에서 원하는 instance를 Owner 프로퍼티를 통해 접근 가능
+		_ownerStateMachine = new StateMachine<Example>( this );
+
+		StartCoroutine( _stateMachine.Coroutine<StartState>() );
+		StartCoroutine( _ownerStateMachine.Coroutine<OwnerStartState>() );
+	}
+
+	/// <summary>
+	/// 기본 State 클래스를 상속받은 StartState
+	/// </summary>
+	private class StartState : State
+	{
+		private int _count = 0;
+
+		protected override void Begin()
+		{
+			_count = 10;
+			// Debug.Log( $"{Owner.Foo}" ); // Owner기능이 없음, 출력 실패
+		}
+
+		protected override void Update()
+		{
+			if( _count-- <= 0 )
+			{
+				Invoke<EndState>();
+			}
+		}
+
+		protected override void End()
+		{
+			Debug.Log( "Start 상태 이탈" );
+		}
+	}
+
+	private class EndState : State
+	{
+		protected override void Begin()
+		{
+			// 처음 상태로 다시 원상복귀
+			Invoke<StartState>();
+		}
+	}
+
+	/// <summary>
+	/// Owner 접근이 가능한 OwnerStartState
+	/// State를 제네릭으로 상속받으면 동작하는 상태머신이 가진 Owner instance를 접근가능
+	/// </summary>
+	private class OwnerStartState : State<Example>
+	{
+		private int _count = 0;
+
+		protected override void Begin()
+		{
+			_count = 10;
+			//Debug.Log( $"{Owner.Foo}" ); // Owner는 Example의 Instance, "Bar" 출력
+			Invoke<OwnerEndState>();
+		}
+
+		protected override void Update()
+		{
+			if( _count-- <= 0 )
+			{
+				Invoke<OwnerEndState>();
+			}
+		}
+
+		protected override void End()
+		{
+			Debug.Log( "OwnerStart 상태 이탈" );
+		}
+	}
+
+	private class OwnerEndState : State<Example>
+	{
+		protected override void Begin()
+		{
+			// 처음 상태로 다시 원상복귀
+			Invoke<OwnerStartState>();
+		}
+	}
+}
---
+++ Assets/PlayerObject.cs
@@ -0,0 +1,219 @@
+﻿using Lofle;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerObject : NetworkBehaviour
+{
+	public enum eSprite
+	{
+		Idle,
+		Run,
+		Jump
+	}
+
+	[SerializeField]
+	private Rigidbody2D _rigidbody = null;
+
+	[SerializeField]
+	private SpriteRenderer _spriteIdle = null;
+	[SerializeField]
+	private SpriteRenderer _spriteRun = null;
+	[SerializeField]
+	private SpriteRenderer _spriteJump = null;
+
+	private StateMachine<PlayerObject> _stateMachine = null;
+
+	[SerializeField]
+	private float _speed = 5.0f;
+	[SerializeField]
+	private float _jump = 10.0f;
+
+	private bool isJump
+	{
+		get
+		{
+			return 0 != _rigidbody.velocity.y;
+		}
+	}
+
+	void Start ()
+	{
+		ShowSprite( eSprite.Idle );
+		if( !isLocalPlayer ) return;
+
+		_stateMachine = new StateMachine<PlayerObject>( this );
+		StartCoroutine( _stateMachine.Coroutine<IdleState>() );
+	}
+
+	[Command]
+	private void CmdJump()
+	{
+		RpcJump();
+	}
+
+	[ClientRpc]
+	private void RpcJump()
+	{
+		if( !isClient ) return;
+		if( !isJump )
+		{
+			_rigidbody.AddForce( new Vector2( 0, _jump ) );
+		}
+	}
+
+	[Command]
+	private void CmdMove( bool bLeft )
+	{
+		RpcMove( bLeft );
+	}
+	[ClientRpc]
+	private void RpcMove( bool bLeft )
+	{
+		if( !isClient ) return;
+		_rigidbody.AddForce( new Vector2( ( bLeft ? -_speed : _speed ), 0 ) );
+	}
+
+	[Command]
+	private void CmdLookAt( bool bLeft )
+	{
+		RpcLookAt( bLeft );
+	}
+	[ClientRpc]
+	private void RpcLookAt( bool bLeft )
+	{
+		if( !isClient ) return;
+		transform.rotation = Quaternion.Euler( new Vector3( transform.rotation.x, bLeft ? 180 : 0, transform.rotation.z ) );
+	}
+
+	private void HideAllSprite()
+	{
+		_spriteIdle.enabled = false;
+		_spriteRun.enabled = false;
+		_spriteJump.enabled = false;
+	}
+
+	[Command]
+	private void CmdShowSprite(eSprite type)
+	{
+		RpcShowSprite( type );
+	}
+
+	[ClientRpc]
+	private void RpcShowSprite( eSprite type )
+	{
+		if( !isClient ) return;
+		ShowSprite( type );
+	}
+
+	private void ShowSprite( eSprite type )
+	{
+		HideAllSprite();
+
+		switch( type )
+		{
+			case eSprite.Idle:
+				_spriteIdle.enabled = true;
+				break;
+
+			case eSprite.Run:
+				_spriteRun.enabled = true;
+				break;
+
+			case eSprite.Jump:
+				_spriteJump.enabled = true;
+				break;
+		}
+	}
+
+	private class IdleState : State<PlayerObject>
+	{
+		protected override void Begin()
+		{
+			Owner.CmdShowSprite( eSprite.Idle );
+		}
+
+		protected override void Update()
+		{
+			if( Input.GetKey( KeyCode.LeftArrow ) || Input.GetKey( KeyCode.RightArrow ) )
+			{
+				Invoke<RunState>();
+			}
+
+			if( Input.GetKey( KeyCode.Space ) )
+			{
+				Invoke<JumpState>();
+			}
+		}
+
+		protected override void End()
+		{
+		}
+	}
+
+	private class RunState : State<PlayerObject>
+	{
+		protected override void Begin()
+		{
+			Owner.CmdShowSprite( eSprite.Run );
+		}
+
+		protected override void Update()
+		{
+			if( Input.GetKey( KeyCode.LeftArrow ) )
+			{
+				Owner.CmdMove( true );
+				Owner.CmdLookAt( true );
+			}
+			else if( Input.GetKey( KeyCode.RightArrow ) )
+			{
+				Owner.CmdMove( false );
+				Owner.CmdLookAt( false );
+			}
+			else
+			{
+				Invoke<IdleState>();
+			}
+
+			if( Input.GetKey( KeyCode.Space ) )
+			{
+				Invoke<JumpState>();
+			}
+		}
+
+		protected override void End()
+		{
+		}
+	}
+
+	private class JumpState : State<PlayerObject>
+	{
+		protected override void Begin()
+		{
+			Owner.CmdJump();
+			Owner.CmdShowSprite( eSprite.Jump );
+		}
+
+		protected override void Update()
+		{
+			if( Input.GetKey( KeyCode.LeftArrow ) )
+			{
+				Owner.CmdMove( true );
+			}
+			else if( Input.GetKey( KeyCode.RightArrow ) )
+			{
+				Owner.CmdMove( false );
+			}
+
+			if(!Owner.isJump)
+			{
+				Invoke<IdleState>();
+			}
+		}
+
+		protected override void End()
+		{
+		}
+	}
+}
